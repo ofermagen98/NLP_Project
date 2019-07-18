b5fce67d87cf5f28769a12150c97bada247756d8
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-07-19 01:02:18 +0300

    genertor

--- generator.py
+++ generator.py
@@ -4,75 +4,139 @@ from tensorflow.python.keras.utils.data_utils import Sequence
 import json
 from random import shuffle as _shuffle
 from copy import deepcopy
+from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def get_images(dir_path,suffix):
+    paths = os.listdir(dir_path)
+    paths = filter(lambda s: s[-len(suffix):] == suffix, paths)
+    paths = map(lambda s: os.path.join(dir_path,s), paths)
+    paths = list(paths)
+    key = lambda s: int(os.path.basename(s)[:-len(suffix)])
+    paths.sort(key = key)
+    images = [np.array(Image.open(im)) for im in paths]
+    images = np.stack(images)
+    return images
 
 class DataGenerator(Sequence):
     '''
     '''
-    def __init__(self, ddir,batch_size = 32,batch_norm = True):
+    def __init__(self, ddir, batch_size = 32, augmentation = True):
         assert os.path.isdir(ddir)
         super(DataGenerator,self).__init__()
         self.ddir = ddir
         self.batch_size = batch_size
-        self.batch_norm = batch_norm
+        self.last_dir = None
 
-        with open(os.path.join(ddir,'params.json'),'r') as f:
-            self.params = json.load(f)
+        self.genL = ImageDataGenerator(
+        # set input mean to 0 over the dataset
+        featurewise_center=True,
+        # set each sample mean to 0
+        samplewise_center=False,
+        # divide inputs by std of dataset
+        featurewise_std_normalization=True,
+        # divide each input by its std
+        samplewise_std_normalization=False,
+        # apply ZCA whitening
+        zca_whitening=False,
+        # epsilon for ZCA whitening
+        zca_epsilon=1e-06,
+        # randomly rotate images in the range (deg 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally
+        width_shift_range=0.1,
+        # randomly shift images vertically
+        height_shift_range=0.1,
+        # set range for random shear
+        shear_range=0.,
+        # set range for random zoom
+        zoom_range=0.,
+        # set range for random channel shifts
+        channel_shift_range=0.,
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        # value used for fill_mode = "constant"
+        cval=0.,
+        # randomly flip images
+        horizontal_flip=True,
+        # randomly flip images
+        vertical_flip=False,
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0)
+        self.genR = deepcopy(self.genL)
 
-        self.sizes = self.params['sizes']
-        self.shuffle()
-        self.loaded_file_idx = -1
+        self.dirnum = os.listdir(ddir)
+        self.dirnum = map(lambda s: os.path.join(ddir,s), self.dirnum)
+        self.dirnum = filter(os.path.isdir, self.dirnum)
+        self.dirnum = len(list(self.dirnum))
 
-    def on_epoch_end(self):
-        self.shuffle()
+        suffix = "-img0.png"
+        def dir_size(i,ddir,suffix):
+            lst = os.listdir(os.path.join(ddir,str(i)))
+            lst = filter(lambda s: s[-len(suffix):] == suffix,lst)
+            return sum(1 for _ in lst)
 
-    def shuffle(self):
-        super_batch_order = list(range(len(self.sizes)))
-        _shuffle(super_batch_order)
+        self.sizes = map(lambda i: dir_size(i,ddir,suffix), range(self.dirnum))
+        self.sizes = list(self.sizes)
 
-        self.file = []
-        self.idxs = []
+        self.len = 0
+        self.files = []
+        self.indexs = []
+        for i,sz in enumerate(self.sizes):
+            for j in range(0,sz,self.batch_size):
+                self.len += 1
+                self.files.append(i)
+                self.indexs.append(j)
+        self.files = np.array(self.files,dtype=np.dtype('int32'))
+        self.indexs = np.array(self.indexs,dtype=np.dtype('int32'))
 
-        for super_batch in super_batch_order:
-            sz =  self.sizes[super_batch]
-            batch_order = list(range(sz))
-            _shuffle(batch_order)
-            for i in range(0,sz,self.batch_size):
-                self.idxs.append(batch_order[i:i+self.batch_size])
-                self.file.append(super_batch)
-    
+        sampled_images = 0
+        sampled_images = os.path.join(ddir,str(sampled_images))
+        sampled_images = get_images(sampled_images,"-img0.png")
+
+        print('fitting generators')
+        self.genL.fit(sampled_images)
+        self.genR.fit(sampled_images)
+
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return len(self.idxs)
+        return self.len
 
-    def load_file(self,j):
-        with open(os.path.join(self.ddir,'imgL-%d.npy' % j),'rb') as f:
-            self.imgL = np.load(f)
-        with open(os.path.join(self.ddir,'imgR-%d.npy' % j),'rb') as f:
-            self.imgR = np.load(f)
-        with open(os.path.join(self.ddir,'sentence-%d.npy' % j),'rb') as f:
-            self.sentence = np.load(f)
-        with open(os.path.join(self.ddir,'label-%d.npy' % j),'rb') as f:
-            self.label = np.load(f)
-        self.loaded_file_idx = j
+    def load_folder(self,j):
+        dir_path = os.path.join(self.ddir,str(j))
+        with open(os.path.join(dir_path,'data.json'), 'r') as f:
+            data = json.load(f)
+            sz = self.sizes[j]
+            self.sentence = [d['sentence'] for d in data]
+            self.sentence = [np.stack(self.sentence[i:i+self.batch_size]) for i in range(0,sz,self.batch_size)]
+
+            self.label = [bool(d['label']) for d in data]
+            self.label = [np.stack(self.label[i:i+self.batch_size]) for i in range(0,sz,self.batch_size)]
+
+        print(self.label)
+        imgL = get_images(dir_path,"-img0.png")
+        self.imgL = self.genL.flow(imgL,y=None,batch_size=self.batch_size,shuffle=False)
+        imgR = get_images(dir_path,"-img1.png")
+        self.imgR = self.genR.flow(imgR,y=None,batch_size=self.batch_size,shuffle=False)
+        self.last_dir  = j
 
     def __getitem__(self, index):
         'Generate one batch of data'
-        if self.loaded_file_idx != self.file[index]:
-            self.load_file(self.file[index])
-        
-        idxs = self.idxs[index]
-        imgL = self.imgL[idxs,:,:,:]
-        imgR = self.imgR[idxs,:,:,:]
-        sentence = self.sentence[idxs,:]
-        label = self.label[idxs]
+        file = self.files[index]
+        if self.last_dir != file: self.load_folder(file)
+        index = self.indexs[index]
+        return [self.imgL[index], self.imgR[index], self.sentence[index]], self.label[index]
 
-        imgL = imgL.astype(np.dtype('float32'))
-        imgR = imgR.astype(np.dtype('float32'))
 
-        if self.batch_norm:
-            imgL -= np.mean(imgL)
-            imgL /= np.linalg.norm(imgL)
-            imgR -= np.mean(imgR)
-            imgR /= np.linalg.norm(imgR)
-            
-        return [imgL,imgR,sentence], label
+print('defining generator')
+
+gen = DataGenerator('/Users/ofermagen/Documents/formatted_images')
+
+print(gen[1][1])
+
