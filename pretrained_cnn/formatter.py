d6770f6b52d07bc694882ea964e414b4e8b36716
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-07-20 14:54:53 +0300

    T

--- pretrained_cnn/formatter.py
+++ pretrained_cnn/formatter.py
@@ -44,13 +44,13 @@ def read_img(path):
 
 #####################
 
-orig_dir = '/Users/ofermagen/Coding/NLP_Project_Data/'
+orig_dir = '/home/ofermagen/'
 assert os.path.isdir(orig_dir)
 json_file = orig_dir + 'nlvr/nlvr2/data/train.json'
 imgs_dir = orig_dir + 'images/train'
 hash_file = orig_dir + 'nlvr/nlvr2/util/hashes/train_hashes.json'
 
-DDIR = '/Users/ofermagen/Coding/NLP_Project_Data/pretraining_data'
+DDIR = '/home/ofermagen/pretraining_data'
 if not os.path.isdir(DDIR): os.mkdir(DDIR)
 #####################
 
