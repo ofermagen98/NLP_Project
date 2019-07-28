55cc7585bd52552c0c6406461758abd06a028a11
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-07-28 18:29:15 +0300

    T

--- model.py
+++ model.py
@@ -56,7 +56,7 @@ model = Model(inputs=[imgL,imgR,sent],outputs=pred)
 model.compile('adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 model_path = '/home/ofermagen/checkpoints/model.{epoch:03d}.h5'
-model.load_weights(model_path)
+#model.load_weights(model_path)
 checkpoint = ModelCheckpoint(filepath=model_path,save_best_only=True)
 
 print('creating generators')
