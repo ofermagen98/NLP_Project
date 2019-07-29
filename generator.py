19a7736a801b084b6241676dc2697dca07d92eb9
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-07-29 19:07:29 +0300

    T

--- generator.py
+++ generator.py
@@ -12,55 +12,60 @@ class DataGenerator(Sequence):
     """
     """
 
-    def __init__(self, ddir, batch_size=40, shuffle=True, augmentation=True):
+    def __init__(self, ddir, batch_size=40, shuffle=True, augmentation=False):
         assert os.path.isdir(ddir)
         super(DataGenerator, self).__init__()
         self.ddir = ddir
         self.batch_size = batch_size
         self.last_dir = None
 
-        self.gen = ImageDataGenerator(
-            # set input mean to 0 over the dataset
-            featurewise_center=True,
-            # set each sample mean to 0
-            samplewise_center=False,
-            # divide inputs by std of dataset
-            featurewise_std_normalization=True,
-            # divide each input by its std
-            samplewise_std_normalization=False,
-            # apply ZCA whitening
-            zca_whitening=False,
-            # epsilon for ZCA whitening
-            zca_epsilon=1e-06,
-            # randomly rotate images in the range (deg 0 to 180)
-            rotation_range=0,
-            # randomly shift images horizontally
-            width_shift_range=0.1,
-            # randomly shift images vertically
-            height_shift_range=0.1,
-            # set range for random shear
-            shear_range=0.0,
-            # set range for random zoom
-            zoom_range=0.0,
-            # set range for random channel shifts
-            channel_shift_range=0.0,
-            # set mode for filling points outside the input boundaries
-            fill_mode="nearest",
-            # value used for fill_mode = "constant"
-            cval=0.0,
-            # randomly flip images
-            horizontal_flip=True,
-            # randomly flip images
-            vertical_flip=False,
-            # set rescaling factor (applied before any other transformation)
-            rescale=None,
-            # set function that will be applied on each input
-            preprocessing_function=None,
-            # image data format, either "channels_first" or "channels_last"
-            data_format=None,
-            # fraction of images reserved for validation (strictly between 0 and 1)
-            validation_split=0.0,
-        )
+        if augmentation:
+            self.gen = ImageDataGenerator(
+                # set input mean to 0 over the dataset
+                featurewise_center=True,
+                # set each sample mean to 0
+                samplewise_center=False,
+                # divide inputs by std of dataset
+                featurewise_std_normalization=True,
+                # divide each input by its std
+                samplewise_std_normalization=False,
+                # apply ZCA whitening
+                zca_whitening=False,
+                # epsilon for ZCA whitening
+                zca_epsilon=1e-06,
+                # randomly rotate images in the range (deg 0 to 180)
+                rotation_range=0,
+                # randomly shift images horizontally
+                width_shift_range=0.1,
+                # randomly shift images vertically
+                height_shift_range=0.1,
+                # set range for random shear
+                shear_range=0.0,
+                # set range for random zoom
+                zoom_range=0.0,
+                # set range for random channel shifts
+                channel_shift_range=0.0,
+                # set mode for filling points outside the input boundaries
+                fill_mode="nearest",
+                # value used for fill_mode = "constant"
+                cval=0.0,
+                # randomly flip images
+                horizontal_flip=True,
+                # randomly flip images
+                vertical_flip=False,
+                # set rescaling factor (applied before any other transformation)
+                rescale=None,
+                # set function that will be applied on each input
+                preprocessing_function=None,
+                # image data format, either "channels_first" or "channels_last"
+                data_format=None,
+                # fraction of images reserved for validation (strictly between 0 and 1)
+                validation_split=0.0,
+            )
+        else:
+            self.gen = ImageDataGenerator(
+                featurewise_center=True, featurewise_std_normalization=True
+            )
 
         self.dir_num = os.listdir(ddir)
         self.dir_num = map(lambda s: os.path.join(ddir, s), self.dir_num)
@@ -118,8 +123,11 @@ class DataGenerator(Sequence):
 
         imgL = map(lambda i: self.paths[i] + "-img0.png", idx)
         imgL = np.stack([np.array(Image.open(path)) for path in imgL])
+        imgL = self.gen.flow(imgL, batch_size=imgL.shape[0], shuffle=False)[0]
+
         imgR = map(lambda i: self.paths[i] + "-img1.png", idx)
         imgR = np.stack([np.array(Image.open(path)) for path in imgR])
+        imgR = self.gen.flow(imgR, batch_size=imgR.shape[0], shuffle=False)[0]
 
         return [imgL, imgR, self.sentences[idx, :]], self.labels[idx]
 
