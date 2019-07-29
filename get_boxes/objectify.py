a88b52ea9a9f9d916e8b55eb8bbfd5e7e9433b4a
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-07-29 23:27:29 +0300

    T

--- get_boxes/objectify.py
+++ get_boxes/objectify.py
@@ -115,7 +115,7 @@ special_classes = 1
 print("formatting")
 res_dict = dict()
 
-for ID in progressbar(id2_image_path):
+for ID in progressbar(id2_image_path.keys()[:100]):
     if ID not in id2_boxes_path:
         continue
     OBJ = id2_objects(ID)
