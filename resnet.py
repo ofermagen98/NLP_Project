4964584fd2bc7f7406b6a3416072d300ca640c55
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-08-05 19:19:21 +0300

    T

--- resnet.py
+++ resnet.py
@@ -73,7 +73,7 @@ class Simple_CNN(tf.keras.layers.Layer):
         img = Input(shape=input_shape, name="img", dtype="float32")
         X = BatchNormalization()(img)
 
-        for kernel_size,num_filters,strides in enumerate(params):
+        for kernel_size,num_filters,strides in params:
             conv = Conv2D(
                 num_filters,
                 kernel_size=kernel_size,
