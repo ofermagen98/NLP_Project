3a9bce7186889dafe7c4caa04b7b6496a2a045a5
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-08-06 09:10:20 +0300

    T

--- LSTM.py
+++ LSTM.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Sequential
 from utils import DROPOUT_RATE
 
 class Encoder(tf.keras.layers.Layer):
-    def __init__(self, units,  input_vocab_size, output_dim = 256, word_dim=300, embeddings_initializer='uniform', **kwargs):
+    def __init__(self, units,  input_vocab_size, size=40, output_dim = 256, word_dim=300, embeddings_initializer='uniform', **kwargs):
         super(Encoder,self).__init__()
         embedding = tf.keras.layers.Embedding(
             input_vocab_size, word_dim, embeddings_initializer=embeddings_initializer
@@ -12,6 +12,7 @@ class Encoder(tf.keras.layers.Layer):
 
         self.model = Sequential()
         self.model.add(embedding)
+        self.model.add(Reshape(size,word_dim,1),input_shape=(size,word_dim))
         self.model.add(Bidirectional(LSTM(units, return_sequences=True), input_shape=(word_dim,1)))
         self.model.add(TimeDistributed(Dense(256, activation='sigmoid')))
     
