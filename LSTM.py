5b1ad6f5f71cf7385e65afbe6801d7b8bc8f7900
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-08-06 16:13:30 +0300

    T

--- LSTM.py
+++ LSTM.py
@@ -5,19 +5,24 @@ from RN import Perceptron
 from utils import DROPOUT_RATE
 
 class Encoder(tf.keras.layers.Layer):
-    def __init__(self, units, prec_params, input_vocab_size, size=40, output_dim = 256, word_dim=300, embeddings_initializer='uniform', **kwargs):
+    def __init__(self, units, prec_params, input_vocab_size, size=40, word_dim=300, embeddings_initializer='uniform', **kwargs):
         super(Encoder,self).__init__()
-        embedding = tf.keras.layers.Embedding(
+        self.embedding = tf.keras.layers.Embedding(
             input_vocab_size, word_dim, embeddings_initializer=embeddings_initializer
         )
-        prec = Perceptron(units,prec_params)
-        self.model = Sequential()
-        self.model.add(embedding)
-        self.model.add(Bidirectional(LSTM(units, return_sequences=True), input_shape=(size,word_dim)))
-        print(self.model.layers[-1].output_shape)
-        self.model.add(prec)
+        self._my_size = size
+        self._my_units = units
+        self._my_output_shape = prec_params[-1][0]
+        self.prec = Perceptron(units,prec_params)
+        self.lstm = Bidirectional(LSTM(units, return_sequences=True), input_shape=(size,word_dim))
+        
 
     def call(self,sent):
-        return self.model(sent)
+        X = self.embedding(sent)
+        X = self.lstm(X)
+        X = tf.keras.backend.reshape(X,(-1,2*self._my_units))
+        X = self.prec(X,(-1,2*self._my_units))
+        X = tf.keras.backend.reshape(X,(-1,self._my_size,self._my_output_shape))
+        return X
 
         
