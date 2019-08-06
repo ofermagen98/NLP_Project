b902a649d11c7040ce7227b57888546d9c3c162b
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-08-06 14:42:02 +0300

    T

--- id2path_generator.py
+++ id2path_generator.py
@@ -3,7 +3,7 @@ import pickle
 import json
 from tqdm import tqdm as progressbar
 
-SDIR = '/home/joberant/home/ofermagen/pretrained_cnn_objects/train/'
+SDIR = '/home/joberant/home/ofermagen/pretrained_cnn_objects/dev/'
 objs = os.listdir(SDIR)
 objs = filter(lambda p: os.path.splitext(p)[1] == '.pickle', objs)
 objs = map(lambda p: os.path.join(SDIR,p), objs)
