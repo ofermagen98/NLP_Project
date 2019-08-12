72b6b7e8df9d58901a9cc71fee5d403481c163c6
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-08-12 10:25:19 +0200

    T

---
+++ get_boxes/formatter.py
@@ -0,0 +1,127 @@
+import json
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm as progressbar
+from PIL import Image
+import imagehash
+
+
+def resize(im, desired_size=256):
+    old_size = im.shape[:2]  # old_size is in (height, width) format
+    ratio = float(desired_size) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+
+    # new_size should be in (width, height) format
+    im = cv2.resize(im, (new_size[1], new_size[0]))
+
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+    left, right = delta_w // 2, delta_w - (delta_w // 2)
+
+    color = [128, 128, 128]
+    new_im = cv2.copyMakeBorder(
+        im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+    )
+    return new_im
+
+
+def read_img(path):
+    try:
+        # check hashes
+        im = Image.open(path)
+        name = os.path.basename(path)
+        real = hashes[name]
+        pred = str(imagehash.average_hash(im))
+        assert real == pred, "bad hash"
+
+        # convert cv2 matrix
+        if im.mode != "RGB":
+            im = im.convert("RGB")
+        im = np.asarray(im)
+        im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+        return resize(im)
+
+    except Exception as ex:
+        print(ex)
+        return None
+
+
+#####################
+
+orig_dir = "/Users/ofermagen/Coding/NLP_Project_Data/data/"
+assert os.path.isdir(orig_dir)
+json_file = orig_dir + "nlvr/nlvr2/data/train.json"
+imgs_dir = orig_dir + "unformatted_images/train"
+hash_file = orig_dir + "nlvr/nlvr2/util/hashes/train_hashes.json"
+synset2num_path = orig_dir + "/synset2num.json"
+
+DDIR = orig_dir + "pretraining_data_formatted/train"
+if not os.path.isdir(DDIR):
+    os.mkdir(DDIR)
+#####################
+
+examples = [json.loads(s) for s in open(json_file).readlines()]
+hashes = json.loads(open(hash_file).read())
+
+#####################
+if not os.path.isfile(synset2num_path):
+    synset2num = set(ex["synset"] for ex in examples)
+    synset2num = {synset: i for i, synset in enumerate(synset2num)}
+
+    with open(synset2num_path, "w") as f:
+        json.dump(synset2num, f)
+else:
+    with open(synset2num_path, "r") as f:
+        synset2num = json.load(f)
+
+id2synet = dict()
+for ex in examples:
+    if ex["synset"] not in synset2num:
+        continue
+    ID = ex["identifier"]
+    ID = "-".join(ID.split("-")[:3])
+    for _ID in [ID + "-img0.png", ID + "-img1.png"]:
+        if _ID not in id2synet:
+            id2synet[_ID] = set()
+        id2synet[_ID].add(synset2num[ex["synset"]])
+
+lens = set(len(k) for _, k in id2synet.items())
+assert lens == {1}
+
+for ID in id2synet:
+    id2synet[ID] = list(id2synet[ID])[0]
+
+#####################
+
+id2path = dict()
+for root, _, files in os.walk(imgs_dir):
+    for file in files:
+        if os.path.splitext(file)[1] == ".png":
+            id2path[file] = os.path.join(root, file)
+
+examples = [json.loads(line) for line in open(json_file).readlines()]
+hashes = json.loads(open(hash_file).read())
+
+res_dict = dict()
+
+for ID in progressbar(id2path):
+    img = read_img(id2path[ID])
+    if img is None:
+        continue
+    C = id2synet[ID]
+    res_dir = os.path.join(DDIR, str(C))
+    if not os.path.isdir(res_dir):
+        os.mkdir(res_dir)
+
+    path = os.listdir(res_dir)
+    path = filter(lambda p: os.path.splitext(p)[1] == ".png", path)
+    path = sum(1 for _ in path)
+    path = os.path.join(res_dir, str(path) + ".png")
+
+    res_dict[ID] = path
+    cv2.imwrite(path, img)
+with open(os.path.join(DDIR, "ID2path.json"), "w") as f:
+    json.dump(res_dict, f)
+
---
+++ get_boxes/objectify.py
@@ -0,0 +1,133 @@
+import pickle
+import cv2
+from PIL import Image
+import numpy as np
+import os
+import json
+from progressbar import progressbar
+from objectify_utils import class2num, special_classes
+
+img_dir = "/home/ofermagen/data/unformatted_images/dev/"
+json_file = "/home/ofermagen/data/pretraining_data_formatted/dev/ID2path.json"
+boxes_dir = "/home/ofermagen/data/pretraining_boxes/dev/"
+res_path = "/home/ofermagen/data/objects/dev/"
+
+assert os.path.isdir(img_dir)
+assert os.path.isdir(boxes_dir)
+assert os.path.isfile(json_file)
+assert not os.path.isdir(res_path)
+os.mkdir(res_path)
+
+
+def crop_object(img, box):
+    x0, y0, x1, y1 = box
+    x0 = int(x0 * img.shape[0])
+    x1 = int(x1 * img.shape[0])
+    y0 = int(y0 * img.shape[1])
+    y1 = int(y1 * img.shape[1])
+    return img[x0:x1, y0:y1]
+
+
+def resize(im, desired_size, color=(128, 128, 128)):
+    old_size = im.shape[:2]  # old_size is in (height, width) format
+    ratio = float(desired_size) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+
+    # new_size should be in (width, height) format
+    im = cv2.resize(im, (new_size[1], new_size[0]))
+
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+    left, right = delta_w // 2, delta_w - (delta_w // 2)
+
+    new_im = cv2.copyMakeBorder(
+        im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+    )
+    return new_im
+
+
+def get_class(c):
+    global class_dict, special_classes
+    if c in class_dict:
+        return class_dict[c]
+    else:
+        return 1
+
+
+def id2_objects(ID):
+    global id2_image_path, id2_boxes_path
+
+    assert ID in id2_image_path and ID in id2_boxes_path
+    size = 128
+    orig_img = Image.open(id2_image_path[ID])
+    orig_img = cv2.cvtColor(np.asarray(orig_img), cv2.COLOR_RGB2BGR)
+    img = resize(orig_img, 1024)
+
+    with open(id2_boxes_path[ID], "rb") as f:
+        OBJ = pickle.load(f)
+
+    images = [resize(orig_img, size)]
+    for box in OBJ["detection_boxes"]:
+        cropped_image = crop_object(img, box)
+        cropped_image = resize(cropped_image, size)
+        images.append(cropped_image)
+
+    images = np.stack(images)
+    boxes = np.concatenate(
+        [np.asarray([[0, 1, 0, 1]], dtype=float), OBJ["detection_boxes"]], axis=0
+    )
+    classes = [0] + [get_class(c) for c in OBJ["detection_classes"]]
+    classes = np.asarray(classes)
+    scores = [1.0] + [s for s in OBJ["detection_scores"]]
+    scores = np.asarray(scores)
+    #idx = [i for i, c in enumerate(classes) if c != -1]
+
+    return images, boxes, classes, scores
+
+
+def format_OBJ(images, boxes, classes, scores):
+    return {"images": images, "boxes": boxes, "classes": classes, "scores": scores}
+
+
+print("getting images paths")
+id2_image_path = dict()
+for root, _, files in os.walk(img_dir):
+    for f in files:
+        if os.path.splitext(f)[1] == ".png":
+            id2_image_path[f] = os.path.join(root, f)
+
+print("getting boxes paths")
+id2_boxes_path = dict()
+with open(json_file, "r") as f:
+    tmp = json.load(f)
+    for ID, path in tmp.items():
+        path = path.split("/")[-2:]
+        path = "/".join(path)
+        path = os.path.splitext(path)[0] + ".pickle"
+        path = os.path.join(boxes_dir, path)
+        id2_boxes_path[ID] = path
+
+class_dict = class2num.copy()
+print("formatting")
+res_dict = dict()
+
+for ID in progressbar(id2_image_path):
+    if ID not in id2_boxes_path:
+        continue
+    OBJ = id2_objects(ID)
+    OBJ = format_OBJ(*OBJ)
+    OBJ["ID"] = ID
+
+    path = str(len(res_dict)) + ".pickle"
+    path = os.path.join(res_path, path)
+    res_dict[ID] = path
+    with open(path, "wb") as f:
+        pickle.dump(OBJ, f, pickle.HIGHEST_PROTOCOL)
+
+# int because json is a jerk
+class_dict = {int(k): i for k, i in class_dict.items()}
+path = os.path.join(res_path, "params.json")
+with open(path, "w") as f:
+    json.dump({"ID2path": res_dict, "class2num": class_dict}, f)
+
